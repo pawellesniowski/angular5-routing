13ec3988d647a3f59cd1b56913c94ff4062037b4
Pawel Lesniowski <pawel_lesniowski@epam.com>
2018-03-15 11:19:21 +0100

    styling active  buttons in navigation

--- src/app/pages/notfound.component.ts
+++ src/app/pages/notfound.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<h1>404 Component</h1>`,
+  template: `
+    <h1>404 Component</h1>
+  <a routerLink="/">Go Home</a>`,
 })
 
 export class NotFoundComponent {
